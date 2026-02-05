12ff0e36d56abcc33c74b42b5e1a01e68e1bb9af
Alex-ex4 <lazarosilva2006@gmail.com>
2026-02-04 21:32:43 -0600

    Actualizando tarea 3

--- T1_EJ3.c
+++ T1_EJ3.c
@@ -16,13 +16,19 @@ int insertar_datos(Alumno *ap);
 void prom_mayor_9(Alumno *ap, int n);
 void prom_9_sems(Alumno *ap, int n);
 
+
 int main(){
     Alumno alums[' '],*ap;
     int op, n=0;    
 
     do{
         system("cls");
-        printf("Ingrese una opcion: ");
+        printf("----MENU PRINCIPAL----\n\n");
+        printf("1. Insertar datos de alumnos\n");
+        printf("2. Alumnos con promedio general mayor a 9\n");
+        printf("3. Alumnos con promedio mayor a 9 en cada semestre\n");
+        printf("4. Salir\n\n");
+        printf("\tIngrese una opcion: ");
         scanf("%d", &op);
         getchar(); 
 
@@ -32,7 +38,8 @@ int main(){
                 break;
             case 2:
             if (n<1){
-                printf("No existen datos de alumnos");
+                printf("\n\tNo existen datos de alumnos\n\n");
+                system("pause");
                 break;
             }
             else{
@@ -41,7 +48,8 @@ int main(){
                 break;
             case 3:
             if (n<1){
-                printf("No existen datos de alumnos");
+                printf("\n\tNo existen datos de alumnos\n\n");
+                system("pause");
                 break;
             }
             else{
@@ -57,37 +65,58 @@ int main(){
 }
 
 int insertar_datos(Alumno *ap){
-    int cont=0,i;
+    int cont=0,i,op;
     char temp[' '];
-    system("cls");
-    printf("Ingrese la matricula del alumno %d: ", cont+1);
-    fflush(stdin);
-    fgets(temp, sizeof(temp), stdin);
-    ap->matr=atoi(temp);
+    do
+    {       
 
-    fflush(stdin);
-    printf("Ingrese nombre del alumno %d: ", cont+1);
-    fgets(ap->nom, sizeof(ap->nom), stdin);
-
-    printf("Ingrese el numero de semestres: ");
-    fflush(stdin);
-    fgets(temp, sizeof(temp), stdin);
-    ap->sems=atoi(temp);
-
-    for(i=0;i<ap->sems;i++){
-        printf("Ingrese la calificacion %d: ", i+1);
+        system("cls");
+        printf("----INSERTAR DATOS DE ALUMNOS----\n\n"); 
+        do
+        {
+            fflush(stdin);
+            printf("\nIngrese la matricula del alumno %d: ", cont+1);
+            fgets(temp, sizeof(temp), stdin);
+            ap->matr=atoi(temp);
+        } while (ap->matr<1);
+        
+        
         fflush(stdin);
-		fgets(temp,sizeof(temp),stdin);
-		ap->califs[i]=atof(temp);
-    }
+        printf("\nIngrese nombre del alumno %d: ", cont+1);
+        fgets(ap->nom, sizeof(ap->nom), stdin);
 
-    fflush(stdin);
-    printf("Ingrese la carrera alumno %d: ", cont+1);
-    fgets(ap->carr, sizeof(ap->carr), stdin);
+        do{
+            printf("\nIngrese el numero de semestres: ");
+            fflush(stdin);
+            fgets(temp, sizeof(temp), stdin);
+            ap->sems=atoi(temp);
+        } while (ap->sems<1 || ap->sems>10);
 
-    cont++;
-    ap++;
+        for(i=0;i<ap->sems;i++){
+            do{
+                printf("\nIngrese la calificacion %d: ", i+1);
+                fflush(stdin);
+                fgets(temp,sizeof(temp),stdin);
+                ap->califs[i]=atof(temp);
+            }while(ap->califs[i]<0 || ap->califs[i]>100);
+        }
 
+        fflush(stdin);
+        printf("\nIngrese la carrera alumno %d: ", cont+1);
+        fgets(ap->carr, sizeof(ap->carr), stdin);
+
+        cont++;
+        ap++;
+        
+        do
+        {
+            fflush(stdin);
+            printf("\n\n\t Desea agregar otro alumno [1-Si/2-No]: ");
+            scanf("%d",&op);
+        } while (op!=1 && op!=2);
+
+    } while (op==1);
+    
     return cont;
 }
 void prom_mayor_9(Alumno *ap, int n){
@@ -108,6 +137,9 @@ void prom_mayor_9(Alumno *ap, int n){
         }
         ap++;
     }
+    if(cont==0){
+        printf("\n\tNo hay alumnos con promedio mayor a 9\n");
+    }
     system("pause");
 }
 void prom_9_sems(Alumno *ap, int n){
@@ -117,7 +149,7 @@ void prom_9_sems(Alumno *ap, int n){
     for(i=0;i<n;i++){
         contsems=0;
         for(j=0;j<ap->sems;j++){
-            if(ap->califs[j>90]){
+            if(ap->califs[j]>90){
                 contsems++;
             }
         }       
@@ -128,6 +160,9 @@ void prom_9_sems(Alumno *ap, int n){
             printf("\tMatricula: %d\n", ap->matr);
         }
         ap++;
+    }
+    if(cont==0){
+        printf("\n\tNo hay alumnos con promedio mayor a 9 en cada semestre\n");
     }
     system("pause");
 }
